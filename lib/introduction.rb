6c47a26d4a5db9e63dc41bd9c6f0999dd171b344
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-01 21:49:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,11 @@ def introduction_with_language (name , language)
 end
 
 def introduction_with_language_optional (name , language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  if language = ""
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  else
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
 end
 
 
